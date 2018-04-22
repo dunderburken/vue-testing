fb951c221f34203042d2f1818d64cfe35f8f9e70
Linus <dunderburken@gmail.com>
2018-04-22 21:57:10 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{$route.params.id}}</h1>
+    <div>Menu</div>
+    <router-link to="/">Hello Wrold</router-link>
+    <router-link v-bind:key="item" v-for="item in [1,2,3,4]" to="{path:'/demo/id' + item}">Demo</router-link>
+    <transition name="fade">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Hej! {{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
